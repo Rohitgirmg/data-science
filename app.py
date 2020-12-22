7d60275c3afffcf6b282cb54a12875bbabdfecfc
Rohitgirmg <45727338+Rohitgirmg@users.noreply.github.com>
2020-12-22 09:32:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,url_for,request
+ from flask import Flask,render_template,url_for,request
 import pandas as pd 
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
